6656a8ed4aab74f603ad152f7def8c2a5237dda5
Ilya Belyakov <darksiders1282@gmail.com>
2023-07-29 18:43:39 +0300

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,16 @@
+// JS fundamentals part 1
 let js = "amazing";
-if (js === "amazing") alert("JS is FUN!");
-
-30 + 49 + 120 - 230;
+if (js === "amazing") console.log("JS is FUN!");
 console.log(30 + 49 + 120 - 230);
+
+let firstName = "Ilya";
+console.log(firstName);
+
+// Values and Variables
+let country = "Россия";
+let continent = "Евразия";
+let population = 146980000;
+console.log(country, continent, population);
+console.log(country);
+console.log(continent);
+console.log(population);
