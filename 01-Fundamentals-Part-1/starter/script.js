ca9bcd827a186b3a094a67853bf1a04ea2de5834
Ilya Belyakov <darksiders1282@gmail.com>
2023-07-30 10:37:21 +0300

    data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,16 +1,44 @@
-// JS fundamentals part 1
-let js = "amazing";
-if (js === "amazing") console.log("JS is FUN!");
-console.log(30 + 49 + 120 - 230);
+// * JS fundamentals part 1
+// ? Values and Variables
+// let js = "amazing";
+// if (js === "amazing") console.log("JS is FUN!");
+// console.log(30 + 49 + 120 - 230);
 
-let firstName = "Ilya";
-console.log(firstName);
+// let firstName = "Ilya";
+// console.log(firstName);
 
-// Values and Variables
-let country = "Россия";
-let continent = "Евразия";
-let population = 146980000;
-console.log(country, continent, population);
-console.log(country);
-console.log(continent);
-console.log(population);
+// let country = "Россия";
+// let continent = "Евразия";
+// let population = 146980000;
+// console.log(country, continent, population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// ? data types
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 2123);
+// console.log(typeof "Ilya");
+
+// javascriptIsFun = "Yes";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let isIsland = "Россия";
+// let language;
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof isIsland);
+// console.log(typeof language);
