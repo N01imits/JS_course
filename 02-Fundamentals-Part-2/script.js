151716f9435cba33c157f3feb48dc01cb19b7ec6
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-08-07 15:49:45 +0300

    function Declarations vs Expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,10 +1,25 @@
-function describeCountry(country, population, capitalCity) {
-	return `${country} has ${population} millions people and its capital city is ${capitalCity}`;
+// function describeCountry(country, population, capitalCity) {
+// 	return `${country} has ${population} millions people and its capital city is ${capitalCity}`;
+// }
+
+// const descriptionRussia = describeCountry("Russia", 145, "Moscow");
+// const descriptionArgentina = describeCountry("Argentina", 46, "Buenos Aires");
+// const describeDenmark = describeCountry("Denmark", 0.6, "Copenhagen");
+// console.log(`${descriptionRussia},
+// ${descriptionArgentina},
+// ${describeDenmark}`);
+
+// ? function Declarations
+function percentageOfWorld1(population) {
+	return (population / 7900) * 100;
 }
 
-const descriptionRussia = describeCountry("Russia", 145, "Moscow");
-const descriptionArgentina = describeCountry("Argentina", 46, "Buenos Aires");
-const describeDenmark = describeCountry("Denmark", 0.6, "Copenhagen");
-console.log(`${descriptionRussia},
-${descriptionArgentina},
-${describeDenmark}`);
+// ? function Expressions
+const percentageOfWorld2 = function (population) {
+	return (population / 7900) * 100;
+};
+
+const percRussia = percentageOfWorld1(145);
+const percDenmark = percentageOfWorld1(0.6);
+const percArgentina = percentageOfWorld1(46);
+console.log(percRussia, percDenmark, percArgentina);
