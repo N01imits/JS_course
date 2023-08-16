2256223555cd62b4dd731fc60fb23fc55bd93759
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-08-16 17:12:41 +0300

    add a buttom 'again'

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -44,3 +44,14 @@ document.querySelector('.check').addEventListener('click', function () {
 		}
 	}
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+	score = 20;
+	secretNumber = Math.trunc(Math.random() * 20) + 1;
+	document.querySelector('.score').textContent = score;
+	document.querySelector('.message').textContent = 'Start guessing...';
+	document.querySelector('.number').textContent = '?';
+	document.querySelector('.guess').value = '';
+	document.querySelector('body').style.backgroundColor = '#222';
+	document.querySelector('.number').style.width = '15rem';
+});
