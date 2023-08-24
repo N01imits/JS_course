8bc3f9b146311d5cc27b086140fcd48936f1cca8
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-08-24 11:06:34 +0300

    add holding current score

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -20,24 +20,51 @@ diceElement.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+	document.getElementById(`current--${activePlayer}`).textContent = 0;
+	currentScore = 0;
+	activePlayer = activePlayer === 0 ? 1 : 0;
+	player0Element.classList.toggle('player--active');
+	player1Element.classList.toggle('player--active');
+};
 // бросок кости
 btnRoll.addEventListener('click', function () {
-	// 1. генерация случайного броска игральной кости
-	const dice = Math.trunc(Math.random() * 6) + 1; // +1 для того чтобы генерация была жо 6 включительно
-	// 2. отображение кости
-	diceElement.classList.remove('hidden');
-	diceElement.src = `dice-${dice}.png`;
-	// 3. сравнение выпавшей кости с единицей: если правда -> переключить игрока
-	if (dice !== 1) {
-		//добавить кость к значению
-		currentScore += dice;
-		document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-	} else {
-		// переключение игрока если dice === 1
-		document.getElementById(`current--${activePlayer}`).textContent = 0;
-		currentScore = 0;
-		activePlayer = activePlayer === 0 ? 1 : 0;
-		player0Element.classList.toggle('player--active');
-		player1Element.classList.toggle('player--active');
+	if (playing) {
+		// 1. генерация случайного броска игральной кости
+		const dice = Math.trunc(Math.random() * 6) + 1; // +1 для того чтобы генерация была жо 6 включительно
+		// 2. отображение кости
+		diceElement.classList.remove('hidden');
+		diceElement.src = `dice-${dice}.png`;
+		// 3. сравнение выпавшей кости с единицей: если правда -> переключить игрока
+		if (dice !== 1) {
+			//добавить кость к значению
+			currentScore += dice;
+			document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+		} else {
+			// переключение игрока если dice === 1
+			switchPlayer();
+		}
+	}
+});
+
+btnHold.addEventListener('click', function () {
+	if (playing) {
+		// 1. добавить текущий счет к счету активного игрока
+		scores[activePlayer] += currentScore;
+		document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+		// 2. проверка счета: пока счет игрока <100 && !==100 игра продолжается
+		//  окончание игры
+		if (scores[activePlayer] >= 20) {
+			// Окончание игры
+			playing = false;
+			document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+			document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+			diceElement.classList.add('hidden');
+		} else {
+			// 3. переключение игрока
+			switchPlayer();
+		}
 	}
 });
