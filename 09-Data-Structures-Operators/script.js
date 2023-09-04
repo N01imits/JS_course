5d46ae65f6d37cab3a62ae84b0ae7774973ffebc
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-09-04 12:59:43 +0300

    Coding Challenge #1

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,6 +49,96 @@ const restaurant = {
 	},
 };
 
+const game = {
+	team1: 'Bayern Munich',
+	team2: 'Borrussia Dortmund',
+	players: [
+		[
+			'Neuer',
+			'Pavard',
+			'Martinez',
+			'Alaba',
+			'Davies',
+			'Kimmich',
+			'Goretzka',
+			'Coman',
+			'Muller',
+			'Gnarby',
+			'Lewandowski',
+		],
+		[
+			'Burki',
+			'Schulz',
+			'Hummels',
+			'Akanji',
+			'Hakimi',
+			'Weigl',
+			'Witsel',
+			'Hazard',
+			'Brandt',
+			'Sancho',
+			'Gotze',
+		],
+	],
+	score: '4:0',
+	scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+	date: 'Nov 9th, 2037',
+	odds: {
+		team1: 1.33,
+		x: 3.25,
+		team2: 6.5,
+	},
+};
+
+// * 1) Create one player array for each team (variables 'players1' and 'players2')
+// const players1 = [...game.players[0]];
+// const players2 = [...game.players[1]];
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// * 2) The first player in any player array is the goalkeeper and the others are field
+//  * players. For Bayern Munich (team 1) create one variable ('gk') with the
+//  * goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+//  * field players
+// const [gk, ...fieldPlayers] = [...game.players[0]];
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// * 3) Create an array 'allPlayers' containing all players of both teams (22 players)
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// * 4) During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// *  new array ('players1Final') containing all the original team1 players plus
+// * 'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// * 5) Based on the game.odds object, create one variable for each odd (called'team1', 'draw' and 'team2')
+// const { team1 } = { ...game.odds }; менее читаемый
+const {
+	odds: { team1, x, team2 },
+} = game;
+console.log(team1, x, team2);
+
+// * 6) Write a function ('printGoals') that receives an arbitrary number of player
+// * names (not an array) and prints each of them to the console, along with the
+// * number of goals that were scored in total (number of player names passed in)
+const printGoals = function (...players) {
+	console.log(players);
+	console.log(`${players.length} goals were scored`);
+};
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+// * 7) The team with the lower odd is more likely to win. Print to the console which
+// * team is more likely to win, without using an if/else statement or the ternary
+// * operator.
+team1 < team2 && console.log(`Team 1 is more likely to win`);
+team1 > team2 && console.log(`Team 2 is more likely to win`);
+
+/* // * Logical Assignment Operators
 const rest1 = {
 	name: 'Capri',
 	// numGuests: 20,
@@ -60,24 +150,24 @@ const rest2 = {
 	owner: 'Giovanni Rossi',
 };
 
-// * оператор ||=
+* оператор ||=
 // rest1.numGuests = rest1.numGuests || 10;
 // rest2.numGuests = rest2.numGuests || 10;
 // rest1.numGuests ||= 10;
 // rest2.numGuests ||= 10;
 
-// * оператор ??= (false = undefined и null)
+* оператор ??= (false = undefined и null)
 rest1.numGuests ??= 10;
 rest2.numGuests ??= 10;
 
-// * оператор &&=
+* оператор &&=
 rest1.owner = rest1.owner && '<ANONIM>';
 rest2.owner = rest2.owner && '<ANONIM>';
 rest2.owner &&= '<anonim>';
 rest1.owner &&= '<anonim>';
 
 console.log(rest1);
-console.log(rest2);
+console.log(rest2); */
 
 /* 
 // * Оператор ??
@@ -89,6 +179,7 @@ console.log(guests);
 const guestsCorrect = restaurant.numGuests ?? 10;
 console.log(guestsCorrect);
  */
+
 /* // * короткие прерывания && и ||
 console.log('---OR---');
 // * любые типы данных, возрващают также любые типы, короткие замыкания, && и ||
@@ -152,7 +243,7 @@ add(...x); // ? в функции мы распаковываем массив, 
 restaurant.orderPizza('mashrooms', 'onion', 'olives', 'spinach');
 restaurant.orderPizza('mashrooms'); */
 
-/* // * оператор ...
+/* // * оператор spread ...
 const arr = [7, 8];
 const badNewArr = [1, 2, 3, arr[0], arr[1]];
 console.log(badNewArr);
