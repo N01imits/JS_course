3c0c1c227ac18a201b617526f9d2e48c72984ea9
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-09-07 12:03:48 +0300

    MAPS

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -67,7 +67,39 @@ const restaurant = {
 	},
 };
 
-//* SETS
+//*MAPS
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+
+rest
+	.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+	.set('open', 11)
+	.set('close', 23)
+	.set(true, 'We are open')
+	.set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(1));
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+const arr = [1, 2];
+
+console.log(rest.has(2));
+console.log(rest.delete(2));
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading'); // ? Интересно
+//? rest.set([1, 2], 'Test');
+//? console.log(rest.get([1, 2])); // undefined
+console.log(rest.size);
+console.log(rest);
+console.log(rest.get(arr));
+// rest.clear();
+
+/* //* SETS
 const ordersSet = new Set(['Pizza', 'Pizza', 'Risotto', 'Pasta', 'Risotto']);
 console.log(ordersSet);
 
@@ -87,7 +119,7 @@ const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Waiter', 'Chef'];
 const staffUnique = [...new Set(staff)]; // spread оператор для преобразования в массив
 console.log(staffUnique);
 
-console.log(new Set(staff).size);
+console.log(new Set(staff).size); */
 
 /* // * Перебор объектов
 //* Property Names (ключи)
