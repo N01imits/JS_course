3aab97079fc8bd9e455004f6c2106bdc9f058669
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-09-12 11:48:29 +0300

    Default Parameters

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const bookings = [];
+
+//? после выхода ES6 можно писать default значения прямо в функции
+const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
+	//? ES5 установка значений по умолчанию
+	// 	numPassengers = numPassengers || 1;
+	// 	price = price || 1;
+
+	const booking = { flightNum, numPassengers, price };
+	console.log(booking);
+	bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 30, 800);
+createBooking('LH123', 20);
