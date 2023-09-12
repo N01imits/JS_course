f0d5a1154351342acc8dd469248b40a329915f5f
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-09-12 17:05:21 +0300

    Functions Accepting Callback Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -19,7 +19,7 @@ createBooking('LH123', 30, 800);
 createBooking('LH123', 20);
  */
 
-//* How passing arguments works: value vs reference
+/* //* How passing arguments works: value vs reference
 const flight = 'LH234';
 const ilya = {
 	name: 'Ilya Belyakov',
@@ -50,7 +50,37 @@ const newPassport = function (person) {
 };
 
 newPassport(ilya);
-checkIn(flight, ilya);
+checkIn(flight, ilya); */
 
 //* first-class and higher-order functions
 // Смотри в тетради, либо в презентации
+
+//* Functions Accepting Callback Functions
+const oneWord = function (str) {
+	return str.replaceAll(' ', '').toLowerCase();
+	// return str.replace(/ /g, '').toLowerCase(); // раньше так было
+};
+
+const upperFirstWord = function (str) {
+	const [first, ...others] = str.split(' ');
+	return [first.toUpperCase(), ...others].join(' ');
+};
+
+//? higher-order function
+const transformer = function (str, fn) {
+	console.log(`Original string: ${str}`);
+	console.log(`Transformed string: ${fn(str)}`);
+
+	console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
+
+transformer('JavaScript is the best', oneWord);
+
+//? в обратные вызовы используются постоянно
+const high5 = function () {
+	console.log('hello');
+};
+
+['Ilya', 'Denis'].forEach(high5);
