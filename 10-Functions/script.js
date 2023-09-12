4c5a5b75ca3bf414573a61770c1585c744d59ac1
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-09-12 14:41:27 +0300

    How passing arguments works: value vs reference

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* //* Default Parameters
 const bookings = [];
 
 //? после выхода ES6 можно писать default значения прямо в функции
@@ -16,3 +17,38 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199 * numP
 createBooking('LH123');
 createBooking('LH123', 30, 800);
 createBooking('LH123', 20);
+ */
+
+/* //* How passing arguments works: value vs reference
+const flight = 'LH234';
+const ilya = {
+	name: 'Ilya Belyakov',
+	passport: 12123123123123,
+};
+
+const checkIn = function (flightNum, passenger) {
+	flightNum = 'LH999';
+	passenger.name = 'Mr.' + passenger.name;
+
+	if (passenger.passport === 12123123123123) {
+		alert('Checked in');
+	} else {
+		alert('Wrong passport');
+	}
+};
+
+// checkIn(flight, ilya);
+// console.log(flight);
+// console.log(ilya);
+
+//? in the same as doing
+// const flightNum = flight;
+// const passenger = ilya;
+
+const newPassport = function (person) {
+	person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(ilya);
+checkIn(flight, ilya);
+ */
