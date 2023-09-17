9baca52c15a9f77a4a610b376478e649d94fa8b9
Ilya Belyakov <darksiders1282@gmail.com>
2023-09-17 16:59:00 +0300

    simple array methods

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -6,31 +6,31 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+	owner: 'Jonas Schmedtmann',
+	movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+	interestRate: 1.2, // %
+	pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+	owner: 'Jessica Davis',
+	movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+	interestRate: 1.5,
+	pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+	owner: 'Steven Thomas Williams',
+	movements: [200, -200, 340, -300, -20, 50, 400, -460],
+	interestRate: 0.7,
+	pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+	owner: 'Sarah Smith',
+	movements: [430, 1000, 700, 50, 90],
+	interestRate: 1,
+	pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -63,14 +63,46 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// LECTURES
+//* LECTURES
 
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+	['USD', 'United States dollar'],
+	['EUR', 'Euro'],
+	['GBP', 'Pound sterling'],
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+//* simple array methods
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//* slice
+console.log(arr.slice(1));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+//* splice
+// console.log(arr.splice(2));
+arr.splice(-1);
+arr.splice(1, 2);
+console.log(arr);
+
+//* reverse
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+//* concat
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//* join
+console.log(letters.join(' - '));
