213f58096a3afdfe14bef8a0fc3a0fb9a053eb94
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-09-19 14:09:07 +0300

    the filter method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -248,3 +248,18 @@ const movementsDescriptions = movements.map(
 
 console.log(movementsDescriptions); 
 */
+
+//* the filter method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(function (mov, i, arr) {
+	return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawls = movements.filter(mov => mov < 0);
+console.log(withdrawls);
