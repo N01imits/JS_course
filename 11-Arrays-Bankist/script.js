05494ff13e55866466ee36267196379dd5231fcc
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-09-21 12:35:47 +0300

    coding challenge #2

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -274,7 +274,7 @@ console.log(withdrawls);
  */
 
 //* the reduce method
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+/* const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // const balance = movements.reduce(function (acc, cur, i, arr) {
 // 	console.log(`iteration №${i}: accumulation = ${acc}, current = ${cur}`);
 // 	return acc + cur;
@@ -293,4 +293,49 @@ const max = movements.reduce((acc, mov) => {
 	if (acc > mov) return acc;
 	else return mov;
 }, movements[0]);
-console.log(max);
+console.log(max); */
+
+//* coding challenge #2
+// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's
+// ages ('ages'), and does the following things in order:
+// 1. Calculate the dog age in human years using the following formula: if the dog is
+// <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,
+// humanAge = 16 + dogAge * 4
+// 2. Exclude all dogs that are less than 18 human years old (which is the same as
+// keeping dogs that are at least 18 years old)
+// 3. Calculate the average human age of all adult dogs (you should already know
+// from other challenges how we calculate averages �)
+// 4. Run the function for both test datasets
+// Test data:
+// § Data 1: [5, 2, 4, 1, 15, 8, 3]
+// § Data 2: [16, 6, 10, 5, 6, 1, 4]
+const dogsAges1 = [5, 2, 4, 1, 15, 8, 3];
+const dogsAges2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = function (dogsAges) {
+	const humanAges = dogsAges.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+	const adults = humanAges.filter(age => age >= 18);
+	console.log(humanAges);
+	console.log(adults);
+
+	// const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+	const average = adults.reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+	return average;
+};
+
+// const calcAverageHumanAge = function (ages) {
+// 	const humanAges = ages.map(age => {
+// 		if (age <= 2) return age * 2;
+// 		else return 16 + age * 4;
+// 	});
+// 	console.log(humanAges);
+// };
+
+const avg1 = calcAverageHumanAge(dogsAges1);
+const avg2 = calcAverageHumanAge(dogsAges2);
+console.log(avg1, avg2);
+
+// const calcDisplayBalance = function (movements) {
+// 	const balance = movements.reduce((acc, mov) => acc + mov, 0);
+// 	labelBalance.textContent = `${balance}€`;
+// };
