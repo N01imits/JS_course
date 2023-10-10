a7b7ca798489e2313df39a154ff280a9c87c0ce1
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-10-10 15:25:50 +0300

    Selecting, Creating, and Deleting Elements

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -29,3 +29,43 @@ document.addEventListener('keydown', function (e) {
 		closeModal();
 	}
 });
+
+//* Selecting, Creating, and Deleting Elements
+//* Выбор элементов
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//* создание и вствка элементов
+// .insertAdjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookied for improved functionality and analytics';
+message.innerHTML =
+	'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.prepend(message);
+// header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+//* удалить элемент
+document
+	.querySelector('.btn--close-cookie')
+	.addEventListener('click', function () {
+		message.remove();
+		// раньше так было, когда небыло remove
+		// message.parentElement.removeChild(message)
+	});
